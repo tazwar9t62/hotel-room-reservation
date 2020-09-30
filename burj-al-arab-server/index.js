0c6fe15178f552d88be9e59436a4abf969b0ddf2
Tazwar <tazwar9t6@gmail.com>
2020-09-30 23:28:32 +0600

    done

--- burj-al-arab-server/index.js
+++ burj-al-arab-server/index.js
@@ -1,14 +1,22 @@
 const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
+const admin = require("firebase-admin");
+require("dotenv").config();
+
+const port = 5000;
 const app = express();
 app.use(cors());
 app.use(bodyParser.json());
-const port = 5000;
+const serviceAccount = require("./configs/burj-al-arab-10bae-firebase-adminsdk-yrbve-1964f9cab7.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://burj-al-arab-10bae.firebaseio.com",
+});
 
 const MongoClient = require("mongodb").MongoClient;
-const uri =
-  "mongodb+srv://9t6:livelovedie@cluster0.dupvj.mongodb.net/hotel-room-management?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.DB_NAME}:${process.env.DB_PASS}@cluster0.dupvj.mongodb.net/hotel-room-management?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
@@ -22,12 +30,38 @@ client.connect((err) => {
     bookings.insertOne(newBooking).then((result) => {
       res.send(result.insertedCount > 0);
     });
-    console.log(newBooking);
+    // console.log(newBooking);
   });
   app.get("/bookings", (req, res) => {
-    bookings.find({ email: req.query.email }).toArray((err, documents) => {
-      res.send(documents);
-    });
+    const bearer = req.headers.authorization;
+    // console.log(req.headers.authorization);
+    if (bearer && bearer.startsWith("Bearer ")) {
+      const idToken = bearer.split(" ")[1];
+      //   console.log({ idToken });
+      // idToken comes from the client app
+      admin
+        .auth()
+        .verifyIdToken(idToken)
+        .then(function (decodedToken) {
+          let uid = decodedToken.uid;
+          const tokenEmail = decodedToken.email;
+          const queryEmail = req.query.email;
+          if (tokenEmail == queryEmail) {
+            bookings.find({ email: queryEmail }).toArray((err, documents) => {
+              res.status(200).send(documents);
+            });
+          } else {
+            res.status(401).send("un-authorized acess!!");
+          }
+          // ...
+        })
+        .catch(function (error) {
+          // Handle error
+          res.status(401).send("un-authorized acess!!");
+        });
+    } else {
+      res.status(401).send("un-authorized acess!!");
+    }
   });
 });
 
--- burj-al-arab/src/components/Bookings/Bookings.js
+++ burj-al-arab/src/components/Bookings/Bookings.js
@@ -6,10 +6,17 @@ const Bookings = () => {
 
   const [bookings, setBookings] = useState([]);
   useEffect(() => {
-    fetch("http://localhost:5000/bookings?email=" + loggedInUser.email)
+    const newLocal = "token";
+    fetch("http://localhost:5000/bookings?email=" + loggedInUser.email, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        authorization: `Bearer ${sessionStorage.getItem(newLocal)}`,
+      },
+    })
       .then((res) => res.json())
       .then((data) => setBookings(data));
-  });
+  }, []);
   return (
     <div>
       <h3>You have {bookings.length} bookings: </h3>
